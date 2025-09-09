8f3251061d81c005ff0fa482563b53b112dee020
li <175247291+chai127@users.noreply.github.com>
2025-09-09 22:02:39 +0530

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+  const sections = document.querySelectorAll('section[id]');
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+      const sectionHeight = current.offsetHeight;
+      const sectionTop = current.offsetTop - 100;
+      const sectionId = current.getAttribute('id');
+
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        document
+          .querySelector('.nav-menu a[href*=' + sectionId + ']')
+          .classList.add('active-link');
+      } else {
+        document
+          .querySelector('.nav-menu a[href*=' + sectionId + ']')
+          .classList.remove('active-link');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', scrollActive);
